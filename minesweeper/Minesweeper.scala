835ef21346bca122cd92c48b076bdf4136b0c512
Richard Guan <rguan72@gmail.com>
2022-03-14 21:52:22 -0400

    Add DFS to minesweeper

--- minesweeper/Minesweeper.scala
+++ minesweeper/Minesweeper.scala
@@ -1,13 +1,79 @@
 // make an entire game with i/o?
 // Can use the element class to assist with i/o
 // print out the board and everything in text lmao
+package minesweeper
+
+import scala.collection.mutable
 
 object MinesweeperDriver:
     def main(args: Array[String]): Unit = 
         println("hello world")
     def henlo: String = "henlo"
 end MinesweeperDriver
+// nice constructor for Minesweeper in Minesweeper driver
 
-class Minesweeper:
-    def cool: String = "cool"
+class Minesweeper(
+    val board: Vector[Vector[Tile]]
+):
+    def width: Int = board(0).size
+    def height: Int = board.size
+    override def toString: String = board.mkString("\n")
+    def == (that: Minesweeper): Boolean = 
+        this.board == that.board
+
+    def isVictorious: Boolean = 
+        val eachTileSuccess = 
+            for 
+                row <- board
+                tile <- row
+            yield 
+                (tile.value != 'b' && tile.revealed) || (tile.value == 'b' && !tile.revealed)
+        eachTileSuccess.forall(_ == true)
+
+    def isLost: Boolean = 
+        val eachTileLost = 
+            for 
+                row <- board
+                tile <- row
+            yield
+                tile.value == 'b' && tile.revealed
+        eachTileLost.exists(_ == true)
+
+    def action(col: Char, row: Int): Minesweeper = 
+        val colIndex: Int = col - 'a'
+        val rowIndex = height - row
+        revealCoordinateAndConnectedZeros(colIndex, rowIndex)
+// flipping rows and columns somewhere
+    def revealCoordinateAndConnectedZeros(colIndex: Int, rowIndex: Int): Minesweeper = 
+        val mutableBoard = board.map(_.to(mutable.ArrayBuffer)).to(mutable.ArrayBuffer)
+        setTileToRevealed(mutableBoard, colIndex, rowIndex)
+        if mutableBoard(rowIndex)(colIndex).value == '0' then
+            val stack = mutable.Stack.empty[(Int, Int)]
+            stack.push((colIndex, rowIndex))
+            while !stack.isEmpty do
+                val (currColIndex, currRowIndex) = stack.pop()
+                for (neighborColIndex, neighborRowIndex) <- getNeighbors(currColIndex, currRowIndex) do
+                    if !mutableBoard(neighborRowIndex)(neighborColIndex).revealed then
+                        setTileToRevealed(mutableBoard, neighborColIndex, neighborRowIndex)
+                        if mutableBoard(neighborRowIndex)(neighborColIndex).value == '0' then
+                            stack.push((neighborColIndex, neighborRowIndex))
+        Minesweeper(mutableBoard.map(_.toVector).toVector)
+
+    def setTileToRevealed(mutableBoard: mutable.ArrayBuffer[mutable.ArrayBuffer[Tile]], colIndex: Int, rowIndex: Int): Unit = 
+        val tile = mutableBoard(rowIndex)(colIndex)
+        mutableBoard(rowIndex)(colIndex) = Tile(tile.value, true)
+
+    def getNeighbors(colIndex: Int, rowIndex: Int): Seq[(Int, Int)] = 
+        val shifts = List((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+        for (colShift, rowShift) <- shifts if inBounds(colIndex + colShift, rowIndex + rowShift) yield
+            (colIndex + colShift, rowIndex + rowShift)
+
+    def inBounds(colIndex: Int, rowIndex: Int): Boolean = 
+        colIndex >= 0 && colIndex < width && rowIndex >= 0 && rowIndex < height
+
 end Minesweeper
+
+case class Tile(
+    value: Char,
+    revealed: Boolean
+)
--- minesweeper/MinesweeperTest.scala
+++ minesweeper/MinesweeperTest.scala
@@ -1,6 +1,85 @@
-import MinesweeperDriver.henlo
+import minesweeper.Minesweeper
+import minesweeper.Tile
 
 // learn how to use testing tools later
 object MinesweeperTest:
     def main(args: Array[String]): Unit = 
-        println(s"${MinesweeperDriver.henlo} world")
+        println("Test indexing")
+        val board0 = Vector(
+            Vector(Tile('1', false), Tile('1', false)),
+            Vector(Tile('1', false), Tile('1', false))
+        )
+        println(s"Expected: Vector(Vector((0, false), (0, false)), Vector((0, true), (0, false)), Actual: ${Minesweeper(board0).action('a', 1)}")
+        val board1 = Vector(
+            Vector(Tile('1', false), Tile('1', false), Tile('1', false)),
+            Vector(Tile('1', false), Tile('1', false), Tile('1', false)),
+            Vector(Tile('1', false), Tile('1', false), Tile('1', false)),
+        )
+        val expected = Vector(
+            Vector(Tile('1', true), Tile('1', false), Tile('1', false)),
+            Vector(Tile('1', false), Tile('1', false), Tile('1', false)),
+            Vector(Tile('1', false), Tile('1', false), Tile('1', false)),
+        )
+        println(s"Expected: $expected, Actual: ${Minesweeper(board1).action('a', 3)}")
+
+        val expected2 = Minesweeper(Vector(
+            Vector(Tile('1', false), Tile('1', false), Tile('1', false)),
+            Vector(Tile('1', false), Tile('1', false), Tile('1', true)),
+            Vector(Tile('1', false), Tile('1', false), Tile('1', false)),
+        ))
+        assert(expected2 == Minesweeper(board1).action('c', 2))
+        // println(s"Expected: $expected2, Actual: ${Minesweeper(board1).action('c', 2)}")
+
+        val board2 = Vector(
+            Vector(Tile('1', true), Tile('1', true), Tile('1', true)),
+            Vector(Tile('1', true), Tile('1', true), Tile('b', false)),
+            Vector(Tile('b', false), Tile('1', true), Tile('1', true)),
+        )
+        assert(Minesweeper(board2).isVictorious)
+
+        val board3 = Vector(
+            Vector(Tile('1', true), Tile('1', true), Tile('1', true)),
+            Vector(Tile('1', true), Tile('1', true), Tile('b', false)),
+            Vector(Tile('b', false), Tile('1', false), Tile('1', true)),
+        )
+        assert(!Minesweeper(board3).isVictorious)
+
+        val board4 = Vector(
+            Vector(Tile('1', true), Tile('1', true), Tile('1', true)),
+            Vector(Tile('1', true), Tile('1', true), Tile('b', true)),
+            Vector(Tile('b', false), Tile('1', true), Tile('1', true)),
+        )
+        assert(!Minesweeper(board4).isVictorious)
+
+        val board5 = Vector(
+            Vector(Tile('1', true), Tile('1', true), Tile('1', true)),
+            Vector(Tile('1', true), Tile('1', true), Tile('b', true)),
+            Vector(Tile('b', false), Tile('1', true), Tile('1', true)),
+        )
+        assert(Minesweeper(board5).isLost)
+
+        val board6 = Vector(
+            Vector(Tile('1', true), Tile('1', true), Tile('1', true)),
+            Vector(Tile('1', true), Tile('1', true), Tile('b', false)),
+            Vector(Tile('b', false), Tile('1', true), Tile('1', true)),
+        )
+        assert(!Minesweeper(board6).isLost)
+
+        val graphBoard = Vector(
+            Vector(Tile('0', false), Tile('0', false), Tile('1', true),  Tile('1', false)),
+            Vector(Tile('1', true), Tile('0', false), Tile('1', false), Tile('b', false)),
+            Vector(Tile('0', false), Tile('1', false), Tile('1', false), Tile('1', false)),
+            Vector(Tile('1', false), Tile('1', false), Tile('1', true), Tile('1', false)),
+            Vector(Tile('b', false), Tile('1', false), Tile('1', true), Tile('1', false)),
+        )
+        val expectedGraph = Minesweeper(Vector(
+            Vector(Tile('0', true), Tile('0', true), Tile('1', true),  Tile('1', false)),
+            Vector(Tile('1', true), Tile('0', true), Tile('1', true), Tile('b', false)),
+            Vector(Tile('0', true), Tile('1', true), Tile('1', true), Tile('1', false)),
+            Vector(Tile('1', true), Tile('1', true), Tile('1', true), Tile('1', false)),
+            Vector(Tile('b', false), Tile('1', false), Tile('1', true), Tile('1', false)),
+        ))
+        assert(Minesweeper(graphBoard).action('a', 5) == expectedGraph)
+
+        println("-----------------")
+        println("ALL TESTS PASS")
