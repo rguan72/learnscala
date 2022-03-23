a87534c1f7659a3d105d171b71966d3a0abad1fa
Richard Guan <rguan72@gmail.com>
2022-03-23 18:36:54 -0400

    Update integration tests

--- minesweeper/MinesweeperTest.scala
+++ minesweeper/MinesweeperTest.scala
@@ -1,6 +1,10 @@
-import minesweeper.Minesweeper
-import minesweeper.Tile
 import scala.util.Random
+import scala.collection.mutable
+import minesweeper.Minesweeper
+import minesweeper.MinesweeperGameLoop
+import minesweeper.MinesweeperOptions
+import minesweeper.Tile
+import java.io._
 
 // learn how to use testing tools later
 
@@ -191,5 +195,107 @@ object MinesweeperTest:
         ))
         assert(Minesweeper(4, 5, 4, Random(1)) == expectedSetup1)
 
+        val printedVals0 = mutable.ArrayBuffer.empty[String]
+        val printDep0 = (x: Any) => { printedVals0.addOne(x.toString); () }
+
+        val readLineDepIt0 = Iterator("a 1", "b 1")
+        val readLineDep0 = () => readLineDepIt0.next()
+
+        val opts0 = MinesweeperOptions(1, 2, 0, 0)
+        MinesweeperGameLoop.play(opts0, printDep0, readLineDep0)
+        val expectedInteg0 = mutable.ArrayBuffer(
+            Vector("1| * *", "   ___", "   a b").mkString("\n"),
+            "\n",
+            "What square do you wish to tap? Type column then row, like b 3: ",
+            "Congrats! You won!",
+            "\n",
+            Vector("1| 0 0", "   ___", "   a b").mkString("\n"),
+            "\n"
+        )
+        assert(printedVals0 == expectedInteg0)
+
+        val printedVals1 = mutable.ArrayBuffer.empty[String]
+        val printDep1 = (x: Any) => { printedVals1.addOne(x.toString); () }
+
+        val readLineDepIt1 = Iterator("b 3", "i 3", "g 11")
+        val readLineDep1 = () => readLineDepIt1.next()
+        val opts1 = MinesweeperOptions(13, 18, 16, 3)
+        MinesweeperGameLoop.play(opts1, printDep1, readLineDep1)
+
+        val test1printboard1 = """13| * * * * * * * * * * * * * * * * * *
+12| * * * * * * * * * * * * * * * * * *
+11| * * * * * * * * * * * * * * * * * *
+10| * * * * * * * * * * * * * * * * * *
+ 9| * * * * * * * * * * * * * * * * * *
+ 8| * * * * * * * * * * * * * * * * * *
+ 7| * * * * * * * * * * * * * * * * * *
+ 6| * * * * * * * * * * * * * * * * * *
+ 5| * * * * * * * * * * * * * * * * * *
+ 4| * * * * * * * * * * * * * * * * * *
+ 3| * * * * * * * * * * * * * * * * * *
+ 2| * * * * * * * * * * * * * * * * * *
+ 1| * * * * * * * * * * * * * * * * * *
+    ___________________________________
+    a b c d e f g h i j k l m n o p q r"""
+        val test1printboard2 = """13| 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+12| 1 1 1 1 1 1 1 2 1 1 0 0 0 0 0 0 1 1
+11| * * * * * * * * * 2 1 0 0 0 0 1 2 *
+10| * * * * * * * * * * 1 0 0 0 0 1 * *
+ 9| * * * * * * * * * * 2 0 0 0 0 1 1 1
+ 8| * * * * * * * * * * 1 0 0 0 0 0 0 0
+ 7| * * * * * * * * * 2 1 0 0 0 0 0 0 0
+ 6| * * * * * 1 1 2 1 1 0 0 1 1 1 0 0 0
+ 5| 1 1 1 1 * 1 0 0 0 0 1 1 2 * 1 0 0 0
+ 4| 0 0 0 1 * 1 1 1 1 0 1 * 2 1 1 0 0 0
+ 3| 0 0 0 1 1 1 1 * 1 0 1 1 1 0 0 0 0 0
+ 2| 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 0 0 0
+ 1| 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    ___________________________________
+    a b c d e f g h i j k l m n o p q r"""
+        val test1printboard3 = """13| 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+12| 1 1 1 1 1 1 1 2 1 1 0 0 0 0 0 0 1 1
+11| * * * * * * * * * 2 1 0 0 0 0 1 2 *
+10| * * * * * * * * * * 1 0 0 0 0 1 * *
+ 9| * * * * * * * * * * 2 0 0 0 0 1 1 1
+ 8| * * * * * * * * * * 1 0 0 0 0 0 0 0
+ 7| * * * * * * * * * 2 1 0 0 0 0 0 0 0
+ 6| * * * * * 1 1 2 1 1 0 0 1 1 1 0 0 0
+ 5| 1 1 1 1 * 1 0 0 0 0 1 1 2 * 1 0 0 0
+ 4| 0 0 0 1 * 1 1 1 1 0 1 * 2 1 1 0 0 0
+ 3| 0 0 0 1 1 1 1 * 1 0 1 1 1 0 0 0 0 0
+ 2| 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 0 0 0
+ 1| 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    ___________________________________
+    a b c d e f g h i j k l m n o p q r"""
+        val test1printboard4 = """13| 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+12| 1 1 1 1 1 1 1 2 1 1 0 0 0 0 0 0 1 1
+11| b * * b * * b * b 2 1 0 0 0 0 1 2 b
+10| * * * * * * * * * b 1 0 0 0 0 1 b *
+ 9| * * * * * * * * * * 2 0 0 0 0 1 1 1
+ 8| * * * * b * * * * b 1 0 0 0 0 0 0 0
+ 7| * * * * * * b * b 2 1 0 0 0 0 0 0 0
+ 6| * b * * * 1 1 2 1 1 0 0 1 1 1 0 0 0
+ 5| 1 1 1 1 * 1 0 0 0 0 1 1 2 b 1 0 0 0
+ 4| 0 0 0 1 b 1 1 1 1 0 1 b 2 1 1 0 0 0
+ 3| 0 0 0 1 1 1 1 b 1 0 1 1 1 0 0 0 0 0
+ 2| 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 0 0 0
+ 1| 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    ___________________________________
+    a b c d e f g h i j k l m n o p q r"""
+        val expectedInteg1 = mutable.ArrayBuffer(
+            test1printboard1,
+            "\n",
+            "What square do you wish to tap? Type column then row, like b 3: ",
+            test1printboard2,
+            "\n",
+            "What square do you wish to tap? Type column then row, like b 3: ",
+            test1printboard3,
+            "\n",
+            "What square do you wish to tap? Type column then row, like b 3: ",
+            "Oops, you lost",
+            "\n",
+            test1printboard4,
+            "\n",
+        )
         println("-----------------")
         println("ALL TESTS PASS")
